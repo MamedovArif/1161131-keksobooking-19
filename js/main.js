2d83ba60b443fe0af381b58fcaff01a4199d3bdf
MamaedovArif <arifmho@yandex.ru>
2020-01-28 17:40:37 +0300

    усовершенствовали

--- js/main.js
+++ js/main.js
@@ -5,67 +5,62 @@ function randomInteger(min, max) {
   return Math.round(rand);
 }
 
-var NUMBER_OF_ADS = 8;
-
 var titles = ['домик', 'ночлег', 'конура', 'нора', 'гнездо', 'облако', 'токио', 'жилье'];
 var types = ['palace', 'flat', 'house', 'bungalo'];
 var times = ['12:00', '13:00', '14:00'];
 
-var getFeatures = function () {
-  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var random = randomInteger(0, features.length - 1);
-  var featuresPart = [];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var getArrayRandomLength = function (arr) {
+  var random = randomInteger(0, arr.length - 1);
+  var part = [];
   for (var i = 0; i <= random; i++) {
-    featuresPart.push(features[i]);
+    part.push(arr[i]);
   }
-  return featuresPart;
+  return part;
 };
 
-var getPhotos = function () {
-  var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var random = randomInteger(0, photos.length - 1);
-  var photosPart = [];
-  for (var i = 0; i <= random; i++) {
-    photosPart.push(photos[i]);
-  }
-  return photosPart;
-};
-
-var renderAd = function (index) {
+var renderAd = function (index, title, type, checkin, checkout, feature, photo, x, y) {
   var mainObject = {
     author: {
       avatar: 'img/avatars/user0' + index + '.png'
     },
     offer: {
-      title: titles[randomInteger(0, titles.length - 1)],
+      title: title,
       address: '',
       price: 0,
-      type: types[randomInteger(0, types.length - 1)],
+      type: type,
       rooms: 0,
       guests: 0,
-      checkin: times[randomInteger(0, times.length - 1)],
-      checkout: times[randomInteger(0, times.length - 1)],
-      features: getFeatures(),
+      checkin: checkin,
+      checkout: checkout,
+      features: feature,
       description: '',
-      photos: getPhotos()
+      photos: photo
     },
     location: {
-      x: randomInteger(0, 500),
-      y: randomInteger(130, 630),
+      x: x,
+      y: y
     }
   };
   return mainObject;
 };
 
-var getArrayOfAds = function () {
+var getArrayOfAds = function (numberOfAds) {
   var mainArray = [];
-  for (var i = 1; i <= NUMBER_OF_ADS; i++) {
-    mainArray.push(renderAd(i));
+  for (var i = 1; i <= numberOfAds; i++) {
+    mainArray.push(renderAd(i, titles[randomInteger(0, titles.length - 1)],
+        types[randomInteger(0, types.length - 1)],
+        times[randomInteger(0, times.length - 1)],
+        times[randomInteger(0, times.length - 1)],
+        getArrayRandomLength(features), getArrayRandomLength(photos),
+        randomInteger(0, 500), randomInteger(130, 630)));
   }
   return mainArray;
 };
 
-var arrayOfAds = getArrayOfAds();
+var arrayOfAds = getArrayOfAds(8);
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
